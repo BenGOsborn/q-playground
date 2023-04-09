3a4880106237bed4fbebd690cf3c669d1c00f0b7
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-04-09 22:03:48 +1000

    Created 'and' and 'or' circuit

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
 from qiskit import QuantumCircuit, Aer, execute
 
+# **** I need a comparison circuit
+# **** I also need an equals circuit
+
+
+def adder_circuit(circuit: QuantumCircuit):
+    # https://www.youtube.com/results?search_query=make+an+adder+circuit
+    pass
+
+
+def or_circuit(circuit: QuantumCircuit, in_bit1: int, in_bit2: int, out_bit: int):
+    circuit.x(in_bit1)
+    circuit.x(in_bit2)
+    circuit.ccx(in_bit1, in_bit2, out_bit)
+    circuit.x(out_bit)
+
+
+def and_circuit(circuit: QuantumCircuit, in_bit1: int, in_bit2: int, out_bit: int):
+    circuit.ccx(in_bit1, in_bit2, out_bit)
+
 
 def build_circuit():
     circuit = QuantumCircuit(7, 2)
