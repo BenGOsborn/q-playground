c75e1d2013a1bf59b006e1f716dac2abb2bc2615
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-04-10 10:22:52 +1000

    Corrected terminology

--- src/circuit_node.py
+++ src/circuit_node.py
@@ -5,8 +5,8 @@ from typing import List
 class CircuitNode:
     def __init__(self, prev: List["CircuitNode"]):
         self.prev = prev
-        self.seen = False
         self.op = None
+        self.out = None  # The output bit of the node
 
     def and_(self, node: "CircuitNode"):
         temp = CircuitNode([self, node])
